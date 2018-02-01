c6f1eb3084267396124cbfa14b7a52b456a299fc
Will <w.kopp@kigroup.de>
2018-02-01 13:32:57 +0100

    Add content

---
+++ BaseContract.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.19;
+
+contract BaseContract { 
+    function doMagic() external pure { /* ...*/}
+}
+
+contract OtherBaseContract { 
+    function doMagicTricks() external pure { /* ...*/}
+}
